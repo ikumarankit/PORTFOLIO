c16ea18f7bf40705e4774be57a824924affd233c
ikumarankit <kumarankit993888@gmail.com>
2026-09-25 19:03:04 +0530

    update resume

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import { FaGithub, FaLinkedin, FaEnvelope, FaDownload } from 'react-icons/fa6'
-import resumePdf from '../assests/resume.pdf'
+import resumePdf from '../assets/resume.pdf'
 import profileImg from '../assets/images/profile.jpeg'
 
 const socialLinks = [
